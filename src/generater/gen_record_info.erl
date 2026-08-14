fe9af1b039267c705f089f889e81374efaa547b5
ssss <kuffios@163.com>
2026-08-14 17:43:06 +0800

    123

--- src/generater/gen_record_info.erl
+++ src/generater/gen_record_info.erl
@@ -12,33 +12,47 @@
 -export([start/1, parse_record/1]).
 
 start(_State) ->
-    Dir = "apps/game_server/include/",
-    {ok, Files} = file:list_dir(Dir),
-    Fun =
-        fun(FileName, {XH, XF, XD}) ->
-            {ok, FileBin} = file:read_file(Dir ++ FileName),
-            case re:run(FileBin, <<"-record\\(.*?\\)\\.">>, [global, dotall, {capture, all, binary}]) of
-                {match, L} ->
-                    RecordList = [parse_record(RecordBin) || [RecordBin] <- L],
-                    XH1 = <<XH/binary, "-include(\"", (list_to_binary(FileName))/binary, "\").\n">>,
-                    {XH1, gen_fields(RecordList, XF), gen_default(RecordList, XD)};
-                nomatch ->
-                    {XH, XF, XD}
-            end
-        end,
-
-    {Headers, Fields, Default} = lists:foldl(Fun, {<<>>, <<>>, <<>>}, lists:sort(Files)),
+    {RecordHeaders, Fields, Default} = scan_dir("apps/game_server/include", true),
+    {ProtoHeaders, _, _} = scan_dir("apps/game/include/proto", false),
     NewFields = binary:part(Fields, 0, size(Fields) - 2),
     NewDefault = binary:part(Default, 0, size(Default) - 2),
-    TarFileBin = <<(header())/binary, Headers/binary, "\n", NewFields/binary, ".\n\n", NewDefault/binary, ".">>,
-%%    ok = file:write_file("test.txt", TarFileBin).
-    write_user_default(Headers),
+    TarFileBin = <<(header())/binary, RecordHeaders/binary, "\n", NewFields/binary, ".\n\n", NewDefault/binary, ".">>,
+    write_user_default(<<RecordHeaders/binary, ProtoHeaders/binary>>),
     ok = file:write_file("apps/game_server/src/utils/record_utils.erl", TarFileBin).
+
+%% GenUtils=true  : include + record_utils fields/default
+%% GenUtils=false : 只往 user_default 加 include
+scan_dir(Dir, GenUtils) ->
+    case file:list_dir(Dir) of
+        {ok, Files} ->
+            HrlFiles = lists:sort([F || F <- Files, filename:extension(F) =:= ".hrl"]),
+            lists:foldl(fun(FileName, Acc) ->
+                fold_hrl(Dir, FileName, GenUtils, Acc)
+            end, {<<>>, <<>>, <<>>}, HrlFiles);
+        {error, enoent} ->
+            {<<>>, <<>>, <<>>}
+    end.
+
+fold_hrl(Dir, FileName, GenUtils, {XH, XF, XD}) ->
+    {ok, FileBin} = file:read_file(filename:join(Dir, FileName)),
+    case re:run(FileBin, <<"-record\\(.*?\\)\\.">>, [global, dotall, {capture, all, binary}]) of
+        {match, L} ->
+            XH1 = <<XH/binary, "-include(\"", (list_to_binary(FileName))/binary, "\").\n">>,
+            case GenUtils of
+                true ->
+                    RecordList = [parse_record(RecordBin) || [RecordBin] <- L],
+                    {XH1, gen_fields(RecordList, XF), gen_default(RecordList, XD)};
+                false ->
+                    {XH1, XF, XD}
+            end;
+        nomatch ->
+            {XH, XF, XD}
+    end.
 
 write_user_default(Headers) ->
     Re = <<"%% include start.*%% include end">>,
     UserDefault = "apps/game_server/src/user_default.erl",
-    ReplaceBin = <<"%% include start\n-include(\"protobuf_pb.hrl\").\n", Headers/binary, "%% include end">>,
+    ReplaceBin = <<"%% include start\n", Headers/binary, "%% include end">>,
 
     {ok, Bin} = file:read_file(UserDefault),
     {match, [TarBin]} = re:run(Bin, Re, [dotall, {capture, all, binary}]),
